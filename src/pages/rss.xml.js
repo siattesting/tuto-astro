8e0dc584249c9909c109e0bfa81a2576af02994d
siat.testing <siat.testing@gmail.com>
2023-08-31 11:03:34 +0000

    Rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function GET() {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My Journey Learning Astor',
-    site: 'https://tuto-astro.netlify.app',
+    site: 'https://tuto-astro-siattesting.netlify.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     language: `<language>en-us</language>`,
   });
